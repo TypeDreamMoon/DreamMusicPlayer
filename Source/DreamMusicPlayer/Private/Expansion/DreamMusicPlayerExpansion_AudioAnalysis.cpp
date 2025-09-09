795d20723de6b341b1d8a52d84b36b284a90356a
TypeDreamMoon <1067823908@qq.com>
2025-09-09 22:29:43 +0800

    音频可视化支持

--- Source/DreamMusicPlayer/Private/Expansion/DreamMusicPlayerExpansion_AudioAnalysis.cpp
+++ Source/DreamMusicPlayer/Private/Expansion/DreamMusicPlayerExpansion_AudioAnalysis.cpp
@@ -4,31 +4,19 @@
 #include "Expansion/DreamMusicPlayerExpansion_AudioAnalysis.h"
 #include "Classes/DreamMusicPlayerComponent.h"
 #include "ConstantQNRT.h"
+#include "DreamMusicPlayerDebugLog.h"
+#include "DreamMusicPlayerLog.h"
 #include "LoudnessNRT.h"
+#include "Engine/Canvas.h"
 #include "ExpansionData/DreamMusicPlayerExpansionData_AudioAnalysis.h"
+#include "Kismet/KismetRenderingLibrary.h"
 
-void UDreamMusicPlayerExpansion_AudioAnalysis::GetAudioNrtData(bool bConstantReverse, TArray<float>& ConstantNrtL, TArray<float>& ConstantNrtR,
+void UDreamMusicPlayerExpansion_AudioAnalysis::GetAudioNrtData(TArray<float>& ConstantNrtL, TArray<float>& ConstantNrtR,
                                                                TArray<float>& ConstantNrtAverage,
                                                                float& OutLoudnessValue)
 {
 	ConstantNrtL = ConstantQDataL;
 	ConstantNrtR = ConstantQDataR;
-	TArray<float> ConstantQData, ConstantQDataAverage;
-	if (bConstantReverse)
-	{
-		ConstantQData.Append(ConstantQDataR);
-		ConstantQData.Append(ConstantQDataL);
-	}
-	else
-	{
-		ConstantQData.Append(ConstantQDataL);
-		ConstantQData.Append(ConstantQDataR);
-	}
-
-	for (int i = 0; i < ConstantQData.Num() / 2; ++i)
-	{
-		ConstantQDataAverage.Add((ConstantQData[i] + ConstantQData[ConstantQData.Num() - i - 1]) / 2);
-	}
 	ConstantNrtAverage = ConstantQDataAverage;
 	OutLoudnessValue = LoudnessValue;
 }
@@ -39,11 +27,50 @@ void UDreamMusicPlayerExpansion_AudioAnalysis::UpdateAudioAnalysisData()
 	{
 		ConstantQ->GetNormalizedChannelConstantQAtTime(MusicPlayerComponent->CurrentDuration, 0, ConstantQDataL);
 		ConstantQ->GetNormalizedChannelConstantQAtTime(MusicPlayerComponent->CurrentDuration, 1, ConstantQDataR);
+
+		ConstantQData.Empty();
+		ConstantQDataAverage.Empty();
+
+		auto Avg = [this]()
+		{
+			for (int i = 0; i < ConstantQData.Num() / 2; ++i)
+			{
+				ConstantQDataAverage.Add((ConstantQData[i] + ConstantQData[ConstantQData.Num() - i - 1]) / 2.f);
+			}
+		};
+
+		switch (AverageType)
+		{
+		case EDreamMusicPlayerExpansion_AudioAnalysis_AverageType::Left:
+			ConstantQDataAverage = ConstantQDataL;
+			break;
+		case EDreamMusicPlayerExpansion_AudioAnalysis_AverageType::Right:
+			ConstantQDataAverage = ConstantQDataR;
+			break;
+		case EDreamMusicPlayerExpansion_AudioAnalysis_AverageType::Left_Right:
+			ConstantQData.Append(ConstantQDataL);
+			ConstantQData.Append(ConstantQDataR);
+			Avg();
+			break;
+		case EDreamMusicPlayerExpansion_AudioAnalysis_AverageType::Right_Left:
+			ConstantQData.Append(ConstantQDataR);
+			ConstantQData.Append(ConstantQDataL);
+			Avg();
+			break;
+		}
 	}
 
 	if (Loudness && Loudness->IsValidLowLevel())
 	{
 		Loudness->GetNormalizedLoudnessAtTime(MusicPlayerComponent->CurrentDuration, LoudnessValue);
+	}
+
+	if (bEnableCreateAnalysisTexture && bIsCreated && Internal_AnalysisTexture)
+	{
+		// fmt: B=1byte G=1byte R=1byte A=1byte 48=Width(Channel) 1=Height
+		uint8* Pixels = BuildPixelArray(ConstantQDataAverage);
+		WriteTextureFromPixel(Pixels, Internal_AnalysisTexture);
+		delete[] Pixels;
 	}
 }
 
@@ -57,11 +84,103 @@ void UDreamMusicPlayerExpansion_AudioAnalysis::BP_Tick_Implementation(const FDre
 	UpdateAudioAnalysisData();
 }
 
+void UDreamMusicPlayerExpansion_AudioAnalysis::BP_MusicStart_Implementation()
+{
+	if (Internal_AnalysisTexture)
+	{
+		// Clear
+		Internal_AnalysisTexture->MarkAsGarbage();
+		Internal_AnalysisTexture = nullptr;
+	}
+
+	if (bEnableCreateAnalysisTexture)
+	{
+		if (UTexture2D* CacheTexture = UTexture2D::CreateTransient(48, 1, PF_B8G8R8A8))
+		{
+			CacheTexture->MipGenSettings = TMGS_NoMipmaps;
+			CacheTexture->Filter = TF_Nearest;
+			CacheTexture->CompressionSettings = TC_EditorIcon;
+
+			CacheTexture->LODGroup = TEXTUREGROUP_UI;
+			CacheTexture->UpdateResource();
+
+			Internal_AnalysisTexture = CacheTexture;
+
+			bIsCreated = true;
+
+			OnAnalysisTextureLoaded.Broadcast(Internal_AnalysisTexture);
+		}
+		else
+		{
+			DMP_LOG_DEBUG_EXPANSION(Log, TEXT("Failed to create texture"));
+		}
+	}
+}
+
+uint8* UDreamMusicPlayerExpansion_AudioAnalysis::BuildPixelArray(const TArray<float>& Data)
+{
+	if (Data.IsEmpty())
+	{
+		return nullptr;
+	}
+
+	// fmt: B=1byte G=1byte R=1byte A=1byte 48=Width(Channel) 1=Height
+	uint8* Pixels = new uint8[48 * 1 * 4];
+	for (int32 y = 0; y < 1; ++y)
+	{
+		for (int32 x = 0; x < 48; ++x)
+		{
+			int32 PixelIdx = y * 48 + x;
+			if (Data.IsValidIndex(PixelIdx))
+			{
+				FColor Color = FLinearColor(Data[PixelIdx], Data[PixelIdx], Data[PixelIdx], 1.0f).ToFColor(false);
+				// BGRA
+				Pixels[4 * PixelIdx] = Color.B; // B
+				Pixels[4 * PixelIdx + 1] = Color.G; // G
+				Pixels[4 * PixelIdx + 2] = Color.R; // R
+				Pixels[4 * PixelIdx + 3] = Color.A; // A
+			}
+			else
+			{
+				DMP_LOG_DEBUG_EXPANSION(Warning, TEXT("Invalid index, %d"), PixelIdx)
+				Pixels[4 * PixelIdx] = 0; // B
+				Pixels[4 * PixelIdx + 1] = 0; // G
+				Pixels[4 * PixelIdx + 2] = 0; // R
+				Pixels[4 * PixelIdx + 3] = 0; // A
+			}
+		}
+	}
+
+	return Pixels;
+}
+
+void UDreamMusicPlayerExpansion_AudioAnalysis::WriteTextureFromPixel(uint8* PixelArray, UTexture2D* Texture2D)
+{
+	FTexturePlatformData* PlatformData = Texture2D->GetPlatformData();
+	if (!PlatformData || !PlatformData->Mips.IsValidIndex(0))
+	{
+		DMP_LOG_DEBUG_EXPANSION(Error, TEXT("Invalid platform data"))
+		delete[] PixelArray;
+	}
+	else
+	{
+		void* TextureData = PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+
+		const int32 Size = sizeof(uint8) * 48 * 1 * 4;
+
+		FMemory::Memcpy(TextureData, PixelArray, Size);
+
+		PlatformData->Mips[0].BulkData.Unlock();
+
+		Texture2D->UpdateResource();
+	}
+}
+
 void UDreamMusicPlayerExpansion_AudioAnalysis::LoadAudioNrt()
 {
 	UDreamMusicPlayerExpansionData_AudioAnalysis* MusicData = CurrentMusicData.GetExpansionData<UDreamMusicPlayerExpansionData_AudioAnalysis>();
 	if (!MusicData) return;
-	
+
 	FSoftObjectPath CQ = MusicData->ConstantQ;
 	FSoftObjectPath LN = MusicData->Loudness;
 	if (CQ.IsValid())
--- Source/DreamMusicPlayer/Public/Expansion/DreamMusicPlayerExpansion_AudioAnalysis.h
+++ Source/DreamMusicPlayer/Public/Expansion/DreamMusicPlayerExpansion_AudioAnalysis.h
@@ -6,6 +6,19 @@
 #include "Classes/DreamMusicPlayerExpansion.h"
 #include "DreamMusicPlayerExpansion_AudioAnalysis.generated.h"
 
+UENUM(BlueprintType)
+enum class EDreamMusicPlayerExpansion_AudioAnalysis_AverageType : uint8
+{
+	// Left Div Right
+	Left_Right UMETA(DisplayName = "Left And Right"),
+	// Right Div Left
+	Right_Left UMETA(DisplayName = "Right And Left"),
+	// Only Left
+	Left UMETA(DisplayName = "Only Left"),
+	// Only Right
+	Right UMETA(DisplayName = "Only Right"),
+};
+
 /**
  * 
  */
@@ -13,12 +26,10 @@ UCLASS(DisplayName = "Audio Analysis")
 class DREAMMUSICPLAYER_API UDreamMusicPlayerExpansion_AudioAnalysis : public UDreamMusicPlayerExpansion
 {
 	GENERATED_BODY()
+public:
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAnalysisTextureLoaded, UTexture2D*, Texture);
 
 public:
-	// Current Music Wave
-	UPROPERTY(BlueprintReadOnly, Category = "State")
-	TObjectPtr<USoundWave> SoundWave;
-
 	// Current Music ConstantQ Data
 	UPROPERTY(BlueprintReadOnly, Category = "State")
 	TObjectPtr<UConstantQNRT> ConstantQ;
@@ -39,19 +50,36 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "State")
 	float LoudnessValue;
 
+	UPROPERTY(BlueprintReadOnly, Category = "State", Transient)
+	TArray<float> ConstantQData;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	TArray<float> ConstantQDataAverage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+	EDreamMusicPlayerExpansion_AudioAnalysis_AverageType AverageType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+	bool bEnableCreateAnalysisTexture = false;
+
 public:
 	/**
 	 * Get Current Duration Music NRT Data
-	 * @param bConstantReverse Reverse Average?
 	 * @param ConstantNrtL Current Music Duration L Channel NRT Data
 	 * @param ConstantNrtR Current Music Duration R Channel NRT Data
 	 * @param ConstantNrtAverage NRT Data After Averaging L And R Channels
 	 * @param OutLoudnessValue Current Music Duration Loudness Value
 	 */
 	UFUNCTION(BlueprintPure, Category = "Functions")
-	void GetAudioNrtData(bool bConstantReverse, TArray<float>& ConstantNrtL, TArray<float>& ConstantNrtR, TArray<float>& ConstantNrtAverage,
-						 float& OutLoudnessValue);
-	
+	void GetAudioNrtData(TArray<float>& ConstantNrtL, TArray<float>& ConstantNrtR, TArray<float>& ConstantNrtAverage,
+	                     float& OutLoudnessValue);
+
+	UFUNCTION(BlueprintPure, Category = "Functions")
+	UTexture2D* GetAnalysisTexture() { return Internal_AnalysisTexture; }
+
+	UPROPERTY(BlueprintAssignable, Category = "Delegate")
+	FOnAnalysisTextureLoaded OnAnalysisTextureLoaded;
+
 protected:
 	/**
 	 * Load NRT Data
@@ -65,4 +93,13 @@ protected:
 
 	virtual void BP_ChangeMusic_Implementation(const FDreamMusicDataStruct& InData) override;
 	virtual void BP_Tick_Implementation(const FDreamMusicLyricTimestamp& InTimestamp, float InDeltaTime) override;
+	virtual void BP_MusicStart_Implementation() override;
+
+	static uint8* BuildPixelArray(const TArray<float>& Data);
+	static void WriteTextureFromPixel(uint8* PixelArray, UTexture2D* Texture2D);
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTexture2D> Internal_AnalysisTexture;
+
+	bool bIsCreated = false;
 };
