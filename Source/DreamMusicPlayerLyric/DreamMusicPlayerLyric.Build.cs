6f505712e295c9cf8694670eb0b2c96ef09840c4
TypeDreamMoon <1067823908@qq.com>
2025-11-08 02:53:20 +0800

    impl dream lyric parser

---
+++ Source/DreamMusicPlayerLyric/DreamMusicPlayerLyric.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class DreamMusicPlayerLyric : ModuleRules
+{
+    public DreamMusicPlayerLyric(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/DreamMusicPlayerLyric/Private/DreamMusicPlayerLyric.cpp
@@ -0,0 +1,17 @@
+﻿#include "DreamMusicPlayerLyric.h"
+
+#define LOCTEXT_NAMESPACE "FDreamMusicPlayerLyricModule"
+
+void FDreamMusicPlayerLyricModule::StartupModule()
+{
+    
+}
+
+void FDreamMusicPlayerLyricModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDreamMusicPlayerLyricModule, DreamMusicPlayerLyric)
