cf52b38af2405e216c2b5dcf1149cb6a8efb9f55
TypeDreamMoon <1067823908@qq.com>
2025-11-08 02:55:45 +0800

    init dream music player network

---
+++ Source/DreamMusicPlayerNetwork/Private/DreamMusicPlayerNetwork.cpp
@@ -0,0 +1,17 @@
+﻿#include "DreamMusicPlayerNetwork.h"
+
+#define LOCTEXT_NAMESPACE "FDreamMusicPlayerNetworkModule"
+
+void FDreamMusicPlayerNetworkModule::StartupModule()
+{
+    
+}
+
+void FDreamMusicPlayerNetworkModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDreamMusicPlayerNetworkModule, DreamMusicPlayerNetwork)
