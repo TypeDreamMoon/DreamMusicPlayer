6cc5fe18fd5b0294e90e49752a681f608655268f
TypeDreamMoon <1067823908@qq.com>
2026-01-10 23:57:00 +0800

    chore(version): 更新插件版本号和歌词解析库版本
    - 将插件主版本从 2.0 更新为 2.1.0
    - 将歌词解析库版本从 1.0.0 更新为 0.2.0
    - 更新歌词解析库内部版本号从 1 到 2

--- Source/DreamMusicPlayer/Public/DreamMusicPlayerVersion.h
+++ Source/DreamMusicPlayer/Public/DreamMusicPlayerVersion.h
@@ -4,6 +4,6 @@ namespace DreamMusicPlayerVersion
 {
 	static const FString PluginVersionName = TEXT("2.1.0");
 	static constexpr int PluginVersion = 210;
-	static const FString LyricParserLibraryVersionName = TEXT("1.0.0");
-	static constexpr int LyricParserLibraryVersion = 1;
+	static const FString LyricParserLibraryVersionName = TEXT("0.2.0");
+	static constexpr int LyricParserLibraryVersion = 2;
 }
